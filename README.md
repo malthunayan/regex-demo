cdfde9dda65f56feeaf0716cf6f4194d3b3141a1
Mohammad Al-Thunayan <malthunayan4@gmail.com>
2020-09-01 20:57:12 +0300

    Demo

--- README.md
+++ README.md
@@ -1,68 +1,48 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# Regular Expression Cheat Sheet
 
-## Available Scripts
+## Tokens
 
-In the project directory, you can run:
+- `.` - Any Character Except New Line
+- `\d` - Digit (0-9)
+- `\D` - Not a Digit (0-9)
+- `\w` - Word Character (a-z, A-Z, 0-9, \_)
+- `\W` - Not a Word Character
+- `\s` - Whitespace (space, tab, newline)
+- `\S` - Not Whitespace (space, tab, newline)
 
-### `yarn start`
+- `\b` - Word Boundary
+- `\B` - Not a Word Boundary
+- `^` - Beginning of a String
+- `$` - End of a String
 
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+- `[]` - Matches Characters in brackets
+- `[^ ]` - Matches Characters NOT in brackets
+- `|` - Either Or
+- `( )` - Group
 
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
+## Quantifiers
 
-### `yarn test`
+- `*` - 0 or More
+- `+` - 1 or More
+- `?` - 0 or One
+- `{3}` - Exact Number
+- `{3,}` - Minimum number (e.g. 3 or More)
+- `{3,4}` - Range of Numbers (Minimum, Maximum)
 
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+## Some Characters to Escape
 
-### `yarn build`
+`[].*?{}\^$|+`
 
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
+## Email Regex
 
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
+`[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}` (use case-insensitive flag)
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+`[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}` (no flag required)
 
-### `yarn eject`
+## Resources
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `yarn build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+- [Regex Playground](https://regex101.com/)
+- [Dummy Data](https://raw.githubusercontent.com/CoreyMSchafer/code_snippets/master/Regular-Expressions/data.txt)
+- [A word on Email Regexes](https://www.regular-expressions.info/email.html)
+- [StackOverflow Password Regex](https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a)
+- [Regex in JavaScript - Mozilla](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions/Cheatsheet)
--- src/App.js
+++ src/App.js
@@ -1,26 +1,118 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [account, setAccount] = useState({ username: "", password: "" });
+  const [errors, setErrors] = useState({ username: false, password: false });
+  const [showPassword, setShowPassword] = useState(true);
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    setAccount((account) => ({ ...account, [name]: value }));
+
+    // Check the validity of the current field being changed,
+    // and set the error state of that field to be true
+    // if the field is invalid.
+    const isInvalid = !target.checkValidity();
+    setErrors((errors) => ({ ...errors, [name]: isInvalid }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // The target on submission is the entire form,
+    // so check if the inputs match the constraints set.
+    // There are other HTML constraints available,
+    // such as max and minLength, but those are
+    // beyond the scope of this demo.
+    if (e.target.checkValidity()) {
+      alert("The form passed our test! :D");
+    } else {
+      alert("The form failed our test! :'(");
+    }
+  };
+
+  const handleErrors = (fieldName) => {
+    // Bootstrap base form class
+    let className = "form-control ";
+
+    // If field is empty return only base-class
+    if (account[fieldName].length === 0) return className.trimEnd();
+
+    // If field is invalid return invalid Bootstrap class
+    if (errors[fieldName]) return className + "is-invalid";
+
+    // Return valid input if input is valid
+    return className + "is-valid";
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="auth-wrapper">
+      <div className="auth-inner">
+        <form onSubmit={handleSubmit} noValidate>
+          <h3>Test Regex</h3>
+
+          <div className="form-group">
+            <label>Username Field</label>
+            <input
+              type="tel"
+              className={handleErrors("username")}
+              onChange={handleChange}
+              placeholder="Enter phone number"
+              // Custom regex pattern for validating Kuwaiti
+              // phone numbers. Written by testing different
+              // phone numbers in the regex playground.
+              pattern="^(\+965)?\s?[2569]\d{3}(-|\s)?\d{4}"
+              name="username"
+              value={account.username}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Password</label>
+            <input
+              // Switch between hidden and text field depending on the state
+              type={showPassword ? "text" : "password"}
+              className={handleErrors("password")}
+              onChange={handleChange}
+              placeholder="Enter password"
+              // Password regex found on StackOverflow, the requirements are:
+              // At least 1 lowercase and 1 uppercase letter, 1 number,
+              // 1 special character, and a minimum of 8 characters long.
+              // Check the README file for the link to the discussion.
+              pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
+              name="password"
+              value={account.password}
+            />
+          </div>
+
+          <div className="form-group">
+            <div className="custom-control custom-checkbox">
+              <input
+                type="checkbox"
+                className="custom-control-input"
+                id="customCheck1"
+                // Change hidden field state
+                onChange={() =>
+                  setShowPassword((showPassword) => !showPassword)
+                }
+                checked={showPassword}
+              />
+              <label className="custom-control-label" htmlFor="customCheck1">
+                Show password
+              </label>
+            </div>
+          </div>
+
+          <input
+            type="submit"
+            className="btn btn-primary btn-block"
+            // Disable the button if the fields are empty
+            disabled={!(account.username.length && account.password.length)}
+          />
+        </form>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
